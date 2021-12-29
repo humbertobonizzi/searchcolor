9a6334d3f1fbbaa56462de9f64f1c15df1ba9a88
Humberto Bonizzi <humbertobonizzi@MacBook-Pro-de-Humberto.local>
2021-12-29 14:22:27 -0300

    Primeiro teste de unidade

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Safari()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Safari()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_classificar_cores(self):
+    #Usuário precisa classificar as cores conforme classificação sazonal
+        self.browser.get('http://localhost:8000')
+
+    # O título da página e o cabeçalho mencionam o formulário de classificação
+        self.assertIn('Classificação de cores', self.browser.title)
+        self.fail('Fim do teste')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
